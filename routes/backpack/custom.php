2f734ab7b7582b7e710c944cb78ddbae922aa195
Prateek Vasireddy <prateek@dave.com>
2020-03-04 15:45:13 -0800

    crud resource and link set up

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -7,8 +7,9 @@
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'prefix' => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
-    'namespace'  => 'App\Http\Controllers\Admin',
+    'namespace' => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    CRUD::resource('aboutpage', 'AboutPageCrudController');
 }); // this should be the absolute last line of this file
