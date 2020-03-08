eb738f7e6353f66e0cf3f9adf7a6fd308f7329a3
Prateek Vasireddy <prateek@dave.com>
2020-03-07 17:40:24 -0800

    new seeder file with a first example seed

---
+++ database/seeds/AboutPage.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AboutPage extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
---
+++ database/seeds/AboutPageSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AboutPageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $culturePage = \App\Models\AboutPage::create([
+            'title' => 'Culture',
+            'header' => 'Our Culture',
+            'content' => 'We aim to build a culture that is inclusive, regenerative and always evolving.'.
+                ' Embodying a true regenerative culture is a necessary commitment in order to create meaningful and lasting change in this world. '.
+                'We acknowledge that we may bring into our movement elements of the toxic system that we are trying to address. '.
+                'Societal systems of oppression, such as sexism, classism and racism, may manifest themselves within our movement. '.
+                '\n\nWe are constantly learning and striving to build a culture that eradicates manifestations of these systems '.
+                'and is inclusive to people of all walks of life. We want people to feel welcome, comfortable and included within our spaces. '.
+                'We want everyone to be heard, listened to, and empowered, regardless of their race, class, gender or other identity.'.
+                '\n\nWe seek to build a culture based on principles from nature and ecology. Its purpose is to nurture a new culture that '.
+                'is resilient and robust and which can support us all through the changes we must inevitably face together. We see that natural cycles of action, '.
+                'followed by rest and reflection are key to sustaining our movement. There is no singular definition, '.
+                'but a framework based on natural principles allows each of us to develop our own approach to practicing a '.
+                'regenerative way of being. At its simplest this means putting a little bit more in than we take out.',
+            'slug' => 'culture',
+        ]);
+    }
+}
