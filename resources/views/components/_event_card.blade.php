4fa3a0f840dc560b675d849ab89049bbc5822ae5
Prateek Vasireddy <prateek@dave.com>
2020-03-10 17:49:52 -0700

    dynamic component in chunks of 3

---
+++ resources/views/components/_event_card.blade.php
@@ -0,0 +1,18 @@
+<div class="col-md mb-3">
+  <div class="block-shadow mb-3">
+    <div class="card border-0 mb-3">
+      <img src="{{ $event->image }}" class="card-img-top" alt="An awesome event!">
+      <div class="card-body">
+        <h3 class="card-title text-dark">{{ $event->name }}</h5>
+          <p class="card-text">{{ $event->description }}</p>
+
+          <div class="d-flex flex-row justify-content-between mb-3">
+            <span><i class="far fa-calendar pr-1"></i>{{ $event->start_date }} / {{ $event->end_date }}</span>
+            <span><i class="fa fa-map-marker-alt pr-1"></i>{{ $event->city }}</span>
+          </div>
+
+          <a href="#" class="btn btn-success px-5">Find out more</a>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,14 +32,14 @@
             <img src="https://picsum.photos/id/1/200/150" class="card-img-top" alt="An awesome event!">
             <div class="card-body">
               <h3 class="card-title text-dark">Lorem ipsum dolor sit amet</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
-              <div class="d-flex flex-row justify-content-between mb-3">
-                <span><i class="far fa-calendar pr-1"></i>02 Feb / 07 Feb</span>
-                <span><i class="fa fa-map-marker-alt pr-1"></i>London</span>
-              </div>
+                <div class="d-flex flex-row justify-content-between mb-3">
+                  <span><i class="far fa-calendar pr-1"></i>02 Feb / 07 Feb</span>
+                  <span><i class="fa fa-map-marker-alt pr-1"></i>London</span>
+                </div>
 
-              <a href="#" class="btn btn-success px-5">Find out more</a>
+                <a href="#" class="btn btn-success px-5">Find out more</a>
             </div>
           </div>
         </div>
