2ca799f4fd8e6b3499e9b9695d9e9bafe77bbb32
bsandusky <brett.sandusky@gmail.com>
2020-03-23 14:03:57 -0400

    add popups when click on map locations

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,39 +5,4 @@
  */
 
 require('./bootstrap')
-
-// Create base map layer
-var map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM(),
-    }),
-  ],
-  view: new ol.View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-})
-
-// Create empty features array
-var features = []
-
-// Loop over groups array, add new map point based on coordinates
-groups.forEach((group) => {
-  features.push(
-    new ol.Feature({
-      geometry: new ol.geom.Point(ol.proj.fromLonLat([group.lng, group.lat])),
-    }),
-  )
-})
-
-// Create new map layer with coordinate points
-var layer = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: features,
-  }),
-})
-
-// Add map layer to view
-map.addLayer(layer)
+require('./map')
