bc13e432c0552e6d5d9d71bcb61600e720e3e90f
Maria <mariahcdcosta@gmail.com>
2020-03-23 20:41:33 +0400

    Add roles and permission constants for backpack user

--- app/Models/BackpackUser.php
+++ app/Models/BackpackUser.php
@@ -12,6 +12,27 @@ class BackpackUser extends User
 
     protected $table = 'users';
 
+    const ROLE_ADMIN = 'admin';
+    const ROLE_CONTENT_WRITER = 'content_writer';
+
+    // Permissions for Users
+    const PERMISSION_USERS_VIEW = 'View Users';
+    const PERMISSION_USERS_CREATE = 'Create Users';
+    const PERMISSION_USERS_EDIT = 'Edit Users';
+    const PERMISSION_USERS_DELETE = 'Delete Users';
+
+    // Permissions for Events
+    const PERMISSION_EVENTS_VIEW = 'View Events';
+    const PERMISSION_EVENTS_CREATE = 'Create Events';
+    const PERMISSION_EVENTS_EDIT = 'Edit Events';
+    const PERMISSION_EVENTS_DELETE = 'Delete Events';
+
+    // Permissions for Pages
+    const PERMISSION_PAGES_VIEW = 'View Pages';
+    const PERMISSION_PAGES_CREATE = 'Create Pages';
+    const PERMISSION_PAGES_EDIT = 'Edit Pages';
+    const PERMISSION_PAGES_DELETE = 'Delete Pages';
+
     /**
      * Send the password reset notification.
      *
