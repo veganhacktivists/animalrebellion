4a6ffeb0315973684cf85e995ed9926cdd8ce33b
Maria <mariahcdcosta@gmail.com>
2020-03-23 20:40:55 +0400

    Update permissions and roles table seeder

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,67 @@
+@@ -0,0 +1,65 @@
+<?php
+
+use App\Models\BackpackUser;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Users CRUD operation permissions
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_USERS_CREATE,
+            'guard_name' => 'backpack',
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_USERS_VIEW,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_USERS_EDIT,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_USERS_DELETE,
+        ]);
+
+        // Event CRUD operation permissions
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_EVENTS_CREATE,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_EVENTS_VIEW,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_EVENTS_EDIT,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_EVENTS_DELETE,
+        ]);
+
+        // Page CRUD operation permissions
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_PAGES_CREATE,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_PAGES_VIEW,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_PAGES_EDIT,
+        ]);
+
+        Permission::create([
+            'name' => BackpackUser::PERMISSION_PAGES_DELETE,
+        ]);
+    }
+}
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\BackpackUser;
 use App\Role;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class RolesTableSeeder extends Seeder
 {
@@ -10,6 +12,32 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'admin']);
+        $adminRole = Role::create(['name' => BackpackUser::ROLE_ADMIN]);
+        $adminPermissionIds = Permission::whereIn('name', [
+            BackpackUser::PERMISSION_USERS_VIEW,
+            BackpackUser::PERMISSION_USERS_CREATE,
+            BackpackUser::PERMISSION_USERS_EDIT,
+            BackpackUser::PERMISSION_USERS_DELETE,
+            BackpackUser::PERMISSION_EVENTS_VIEW,
+            BackpackUser::PERMISSION_EVENTS_CREATE,
+            BackpackUser::PERMISSION_EVENTS_EDIT,
+            BackpackUser::PERMISSION_EVENTS_DELETE,
+            BackpackUser::PERMISSION_PAGES_VIEW,
+            BackpackUser::PERMISSION_PAGES_CREATE,
+            BackpackUser::PERMISSION_PAGES_EDIT,
+            BackpackUser::PERMISSION_PAGES_DELETE,
+        ])->pluck('id')->toArray();
+        $adminRole->permissions()->attach($adminPermissionIds);
+
+        $writerRole = Role::create(['name' => BackpackUser::ROLE_CONTENT_WRITER]);
+        $writerPermissionIds = Permission::whereIn('name', [
+            BackpackUser::PERMISSION_EVENTS_VIEW,
+            BackpackUser::PERMISSION_EVENTS_CREATE,
+            BackpackUser::PERMISSION_EVENTS_EDIT,
+            BackpackUser::PERMISSION_PAGES_VIEW,
+            BackpackUser::PERMISSION_PAGES_CREATE,
+            BackpackUser::PERMISSION_PAGES_EDIT
+        ])->pluck('id')->toArray();
+        $writerRole->permissions()->attach($writerPermissionIds);
     }
 }
