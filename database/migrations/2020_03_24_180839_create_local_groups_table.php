08ce770bfed0a86a89f2e79bd13600f296792f11
bsandusky <brett.sandusky@gmail.com>
2020-03-24 14:10:33 -0400

    update migration for latest timestamp

--- database/migrations/2020_03_24_180839_create_local_groups_table.php
+++ database/migrations/2020_03_24_180839_create_local_groups_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateLocalGroupsTable extends Migration
 {
@@ -13,7 +14,7 @@ class CreateLocalGroupsTable extends Migration
     public function up()
     {
         Schema::create('local_groups', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('address1');
             $table->string('address2')->nullable();
