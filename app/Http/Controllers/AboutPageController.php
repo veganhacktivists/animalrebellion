207e33f09413a7e929477cf345400a9f1ca96918
Prateek Vasireddy <prateek@dave.com>
2020-03-29 13:59:57 -0700

    non-published pages are not visible by default

--- app/Http/Controllers/AboutPageController.php
+++ app/Http/Controllers/AboutPageController.php
@@ -8,6 +8,8 @@ class AboutPageController extends Controller
 {
     public function show(AboutPage $aboutPage)
     {
+        abort_unless($aboutPage->published, 404);
+
         $otherPages = AboutPage::where('id', '!=', $aboutPage->id)->get();
 
         return view('about.show', compact('aboutPage', 'otherPages'));
--- tests/Feature/AboutPageTest.php
+++ tests/Feature/AboutPageTest.php
@@ -4,17 +4,37 @@ namespace Tests\Feature;
 
 use App\Models\AboutPage;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AboutPageTest extends TestCase
 {
+    use RefreshDatabase; // run migrations with this test
+
     public function testIsViewableBySlug()
     {
-        $page = factory(AboutPage::class)->create();
+        $page = factory(AboutPage::class)->create(['published' => true]);
+
+        $this->assertTrue($page->published);
 
         $response = $this->get('/about/'.$page->slug);
 
         $response->assertStatus(200);
+        $response->assertSee($page->title);
+        $response->assertSee($page->header);
+        $response->assertSee($page->slug);
+    }
+
+    public function testIsNotViewableUnlessPublished()
+    {
+        $page = factory(AboutPage::class)->create();
+
+        $this->assertNotTrue($page->published);
+
+        $response = $this->get('/about/'.$page->slug);
+
+        $response->assertStatus(404);
+        $response->assertDontSee($page->title);
+        $response->assertDontSee($page->header);
+        $response->assertDontSee($page->slug);
     }
 }
