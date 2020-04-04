1ed2f50b6ddccb0ba5f6158ab6252d3fbcc3ab41
bsandusky <brett.sandusky@gmail.com>
2020-04-04 15:55:41 -0400

    add social links to popups on map

--- resources/js/map.js
+++ resources/js/map.js
@@ -19,14 +19,16 @@ var map = new ol.Map({
 // Create empty features array
 var features = []
 
-console.log(groups)
-
 // Loop over groups array, add new map point based on coordinates
 groups.forEach((group) => {
   features.push(
     new ol.Feature({
       geometry: new ol.geom.Point(ol.proj.fromLonLat([group.lng, group.lat])),
       name: group.name,
+      website: group.website_url,
+      facebook: group.facebook_url,
+      instagram: group.instagram_url,
+      twitter: group.twitter_url,
     }),
   )
 })
@@ -55,6 +57,38 @@ var popup = new ol.Overlay({
 })
 map.addOverlay(popup)
 
+var constructPopoverContent = function(feature) {
+  content = `<div><strong>${feature.get('name')}</strong><div>`
+
+  if (feature.get('website')) {
+    content += `<a href="${feature.get(
+      'website',
+    )}" target="_blank"><span class="social-icons mr-2"><i class="fas fa-link"></i></span></a>`
+  }
+
+  if (feature.get('facebook')) {
+    content += `<a href="${feature.get(
+      'facebook',
+    )}" target="_blank"><span class="social-icons mr-2"><i class="fab fa-facebook"></i></span></a>`
+  }
+
+  if (feature.get('instagram')) {
+    content += `<a href="${feature.get(
+      'instagram',
+    )}" target="_blank"><span class="social-icons mr-2"><i class="fab fa-instagram"></i></span></a></li>`
+  }
+
+  if (feature.get('twitter')) {
+    content += `<a href="${feature.get(
+      'twitter',
+    )}" target="_blank"><span class="social-icons mr-2"><i class="fab fa-twitter"></i></span></a></li>`
+  }
+
+  content += `</div>`
+
+  return content
+}
+
 // display popup on click
 map.on('click', function(evt) {
   var feature = map.forEachFeatureAtPixel(evt.pixel, function(feature) {
@@ -63,12 +97,16 @@ map.on('click', function(evt) {
 
   // if click is on existing point on map
   if (feature) {
+    // dispose of any open popovers already on the layer
+    $(element).popover('dispose')
+
+    // get coords and construct popover
     var coordinates = feature.getGeometry().getCoordinates()
     popup.setPosition(coordinates)
     $(element).popover({
       placement: 'top',
       html: true,
-      content: feature.get('name'),
+      content: constructPopoverContent(feature),
     })
     $(element).popover('show')
   } else {
