ea2ca88d41c1d6429ab250b77ded9d2f8e5cf9ec
Prateek Vasireddy <prateek@dave.com>
2020-04-07 17:56:51 -0700

    allow non-dynamic content to be translated

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Spatie\CookieConsent\CookieConsentMiddleware::class,
+        \App\Http\Middleware\SetLocale::class,
     ];
 
     /**
@@ -36,7 +37,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\SetLocale::class,
         ],
 
         'api' => [
