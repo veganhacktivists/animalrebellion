b73eb1eb259109de30ff680e40e40b9dcf047544
Maria <mariahcdcosta@gmail.com>
2020-04-13 20:35:40 +0400

    Improve events table seeder

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -13,58 +13,8 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-        Event::create([
-            'name' => 'Lorem ipsum dolor sit amet',
-            'start_date' => Carbon::now()->format('Y-m-d'),
-            'end_date' => (Carbon::now())->addDay()->format('Y-m-d'),
-            'start_time' => Carbon::now()->format('H:i:s'),
-            'end_time' => (Carbon::now())->addDay()->format('H:i:s'),
-            'address' => 'Event Address',
-            'city' => 'London',
-            'country' => 'United Kingdom',
-            'type' => Event::TYPE_ACTIVITY,
-            'hosted_by' => 'Event Host',
-            'short_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'full_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'image' => 'https://animalrebellion.org/wp-content/uploads/2020/02/Animal-Logo-No-Background-1-1536x855.png',
-            'slug' => 'event-slug'
-        ]);
-
-        Event::create([
-            'name' => 'Lorem ipsum dolor sit amet',
-            'start_date' => Carbon::now()->format('Y-m-d'),
-            'end_date' => (Carbon::now())->addDays(2)->format('Y-m-d'),
-            'start_time' => Carbon::now()->format('H:i:s'),
-            'end_time' => (Carbon::now())->addDay()->format('H:i:s'),
-            'address' => 'Event Address',
-            'city' => 'London',
-            'country' => 'United Kingdom',
-            'type' => Event::TYPE_MEETING,
-            'hosted_by' => 'Event Host',
-            'short_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'full_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'image' => 'https://animalrebellion.org/wp-content/uploads/2020/02/Animal-Logo-No-Background-1-1536x855.png',
-            'slug' => 'event-slug-1'
-        ]);
-
-        Event::create([
-            'name' => 'Lorem ipsum dolor sit amet',
-            'start_date' => Carbon::now()->subDays(2)->format('Y-m-d'),
-            'end_date' => (Carbon::now())->format('Y-m-d'),
-            'start_time' => Carbon::now()->format('H:i:s'),
-            'end_time' => (Carbon::now())->addDay()->format('H:i:s'),
-            'address' => 'Event Address',
-            'city' => 'London',
-            'country' => 'United Kingdom',
-            'type' => Event::TYPE_TALK,
-            'hosted_by' => 'Event Host',
-            'short_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'full_description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'image' => 'https://animalrebellion.org/wp-content/uploads/2020/02/Animal-Logo-No-Background-1-1536x855.png',
-            'slug' => 'event-slug-2'
-        ]);
+        factory(Event::class, 2)->state('meeting')->create();
+        factory(Event::class, 2)->state('training')->create();
+        factory(Event::class, 2)->state('talk')->create();
     }
 }
