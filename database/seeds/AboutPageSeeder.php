d2c5be7188871437e209da22a3c123bb55dbcf1f
Prateek Vasireddy <prateek@dave.com>
2020-05-24 15:21:53 -0700

    draft demands page

--- database/seeds/AboutPageSeeder.php
+++ database/seeds/AboutPageSeeder.php
@@ -69,7 +69,7 @@ class AboutPageSeeder extends Seeder
         $valuesPage = \App\Models\AboutPage::create([
             'title' => 'Values',
             'header' => 'Our Principles & Values',
-            'content' => '<h4>All are welcome who want to adhere to our principles and values: </h4><br><br><br><br>'.
+            'content' => '<h4>All are welcome who want to adhere to our principles and values: </h4><br><br>'.
                 '<strong>1. We are an anti-speciesist movement with a shared vision of change</strong> <br><br>'.
                 '<strong>2. We set our mission on what is necessary</strong><br><br>'.
                 '<strong>3. We need a regenerative culture</strong><br><br>'.
@@ -80,6 +80,42 @@ class AboutPageSeeder extends Seeder
                 '<strong>8. We avoid blaming and shaming</strong><br><br>'.
                 '<strong>9. We are a non-violent network</strong><br><br>'.
                 '<strong>10. We are based on autonomy and decentralisation</strong>',
+            'slug' => 'values',
+            'thumbnail' => 'fas fa-comments',
+            'published' => true,
+        ]);
+
+        $demandsPage = \App\Models\AboutPage::create([
+            'title' => 'Demands',
+            'header' => 'Our Demands',
+            'content' => '<h4>Animal Rebellion shares Extinction Rebellion’s Three Demands.<br>'.
+                'To each we add our own: transition to a just and sustainable plant-based food system.</h4><br><br>'.
+                '<strong>1. TELL THE TRUTH</strong> <br>'.
+                '<i>Government must tell the truth by declaring a climate and ecological emergency, working with '.
+                'other institutions to communicate the urgency for change.</i><br>'.
+                'To tell the truth about the climate and ecological emergency (CEE) government, '.
+                'media, and all other institutions must tell the truth about animal agriculture. '.
+                'The CEE cannot be addressed effectively without acknowledging the devastating '.
+                'impacts of animal agriculture on the planet. These include, but are not limited to, '.
+                'deforestation, ocean dead-zones, biodiversity loss and air pollution. '.
+                'Without mentioning a plant-based food system, the true solution will always be silenced.<br><br>'.
+                '<strong>2. ACT NOW</strong><br>'.
+                '<i>Government must act now to halt biodiversity loss and reduce greenhouse gas emissions to net zero by 2025.</i><br>'.
+                'We are all hurtling towards climate catastrophe, social collapse, mass starvation, and the death of billions of beings.'.
+                ' To halt this devastation, the government must reduce greenhouse gas (GHG) emissions to net zero by 2025. '.
+                'This cannot be done without transition to a just and sustainable plant-based food system.<br><br>'.
+                '<strong>3. BEYOND POLITICS</strong><br><br>'.
+                '<i>Government must create and be led by the decisions of a Citizens’ Assembly on climate and ecological justice.</i><br>'.
+                'This is an emergency. The issues are huge and complex, and the solutions needed are urgent. '.
+                'Political power in the UK lies in the hands of a few elected politicians. Over the last 40 years, '.
+                'this system has proved incapable of making the long-term decisions needed to '.
+                'deal with the climate and ecological emergency.<br>'.
+                'The political timescale is short-term: intimately linked to electoral, rather than environmental, change. '.
+                'Politicians are also lobbied by huge corporate interests to protect certain industries, '.
+                'including the animal farming and fishing industries.<br>'.
+                'It is therefore crucial that a citizens’ assembly is presented with the facts about animal '.
+                'agriculture and the plant-based food alternatives. Members of the assembly need to make '.
+                'educated and unbiased choices. People must have the power to make informed legislative change.',
             'slug' => 'values',
             'thumbnail' => 'fas fa-comments',
             'published' => true,
