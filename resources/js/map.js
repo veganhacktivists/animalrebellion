ca6693f435f82bdc699d7338a27456f85a4e9f04
bsandusky <brett.sandusky@gmail.com>
2020-05-25 15:20:36 -0400

    add icons to local groups map and style popovers similar to menu dropdowns

--- resources/js/map.js
+++ resources/js/map.js
@@ -25,16 +25,28 @@ function createBaseMapLayer() {
 function loadLocationIcons(locations) {
   var features = []
 
+  var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+      scale: 0.4,
+      src: '/storage/images/local-groups/AR_black_logo_100px.png',
+    }),
+  })
+
   // Loop over locations array, add new map point based on coordinates
   locations.forEach((location) => {
-    features.push(
-      new ol.Feature({
-        geometry: new ol.geom.Point(
-          ol.proj.fromLonLat([location.lng, location.lat]),
-        ),
-        name: location.name,
-      }),
-    )
+    var feature = new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat([location.lng, location.lat]),
+      ),
+      name: location.name,
+      website: location.website_url,
+      facebook: location.facebook_url,
+      instagram: location.instagram_url,
+      twitter: location.twitter_url,
+    })
+
+    feature.setStyle(iconStyle)
+    features.push(feature)
   })
 
   // Create new map layer with coordinate points
@@ -58,9 +70,41 @@ function loadLocationPopups() {
     element: element,
     positioning: 'bottom-center',
     stopEvent: false,
-    offset: [0, -10],
+    offset: [0, -20],
   })
   map.addOverlay(popup)
+
+  var constructPopoverContent = function(feature) {
+    var content = `<div><strong>${feature.get('name')}</strong><div>`
+
+    if (feature.get('website')) {
+      content += `<a href="${feature.get(
+        'website',
+      )}" target="_blank"><span class="social-icons mr-2"><i class="fas fa-link"></i></span></a>`
+    }
+
+    if (feature.get('facebook')) {
+      content += `<a href="${feature.get(
+        'facebook',
+      )}" target="_blank"><span class="social-icons mr-2"><i class="fab fa-facebook"></i></span></a>`
+    }
+
+    if (feature.get('instagram')) {
+      content += `<a href="${feature.get(
+        'instagram',
+      )}" target="_blank"><span class="social-icons mr-2"><i class="fab fa-instagram"></i></span></a></li>`
+    }
+
+    if (feature.get('twitter')) {
+      content += `<a href="${feature.get(
+        'twitter',
+      )}" target="_blank"><span class="social-icons mr-2"><i class="fab fa-twitter"></i></span></a></li>`
+    }
+
+    content += `</div>`
+
+    return content
+  }
 
   // display popup on click
   map.on('click', function(evt) {
@@ -76,7 +120,7 @@ function loadLocationPopups() {
       $(element).popover({
         placement: 'top',
         html: true,
-        content: feature.get('name'),
+        content: constructPopoverContent(feature),
       })
       $(element).popover('show')
     } else {
