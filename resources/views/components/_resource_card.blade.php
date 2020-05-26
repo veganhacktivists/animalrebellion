69b733746300e002aafedbfe17ed609075051e18
bsandusky <brett.sandusky@gmail.com>
2020-05-26 16:08:20 -0400

    show resource details with modal

--- resources/views/components/_resource_card.blade.php
+++ resources/views/components/_resource_card.blade.php
@@ -1,15 +1,25 @@
-<div class="card resource-card">
+<div id="{{$item->id}}" class="card resource-card">
   <h5 class="card-header">{{ $item->title }}</h5>
+
   <div class="card-body">
+    @if(!empty($item->blurb))
     <p class="card-text">{{ $item->blurb }}</p>
+    @endif
+
+    <div class="text-right">
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#resourceModal-{{$item->id}}">
+        Learn More
+      </button>
+    </div>
   </div>
 
-  @if(count($item->tags) > 0)
-    <div class="card-footer text-muted">
+  <div class="card-footer text-muted">
+    <span class="badge badge-dark type-badges">{{ $item->item_type->name }}</span>
+    @if(count($item->tags) > 0)
     @foreach($item->tags as $tag)
-      <span class="badge badge-pill badge-dark tag-badges">{{ $tag->name }}</span>
+    <span class="badge badge-pill badge-dark tag-badges">{{ $tag->name }}</span>
     @endforeach
-    </div>
-  @endif
-
+    @endif
+  </div>
 </div>
--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 <div class="container">
+
   <div class="row">
     <div class="col-12 pt-5 text-center">
       <h1>Resources</h1>
@@ -13,7 +14,8 @@
     <div class="card-columns">
       <ul>
         @foreach($items as $item)
-          @include('components._resource_card', ['item' => $item])
+        @include('components._resource_modal', ['item' => $item])
+        @include('components._resource_card', ['item' => $item])
         @endforeach
       </ul>
     </div>
