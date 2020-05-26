9dc9f95bab4335d07dfdba7b901986dfc38a1af3
Prateek Vasireddy <prateek@dave.com>
2020-05-25 17:04:50 -0700

    configure the crud panel

--- app/Http/Controllers/Admin/BlogPostCrudController.php
+++ app/Http/Controllers/Admin/BlogPostCrudController.php
@@ -32,8 +32,60 @@ class BlogPostCrudController extends CrudController
         |--------------------------------------------------------------------------
         */
 
-        // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
+        // Columns when browsing
+        $this->crud->addColumn('title');
+        $this->crud->addColumn('header');
+        $this->crud->addColumn(['name' => 'published', 'label' => 'Publicly visible', 'type' => 'boolean']);
+
+        // Fields on the edit page
+        $this->crud->addField([
+            'name' => 'title',
+            'type' => 'text',
+            'label' => 'Title',
+            'attributes' => [
+                'placeholder' => 'Page title',
+            ],
+        ]);
+
+        $this->crud->addField([
+            'name' => 'slug',
+            'type' => 'text',
+            'label' => 'Slug (the identifier that shows up in the page\'s URL',
+            'attributes' => [
+                'placeholder' => 'Page slug',
+            ],
+        ]);
+
+        $this->crud->addField([
+            'name' => 'header',
+            'type' => 'text',
+            'label' => 'Header (the bolded text users see at the top of the page',
+            'attributes' => [
+                'placeholder' => 'Page header',
+            ],
+        ]);
+
+        $this->crud->addField([
+            'name' => 'content',
+            'type' => 'summernote',
+            'label' => 'Content (the textual body of the page)',
+            'attributes' => [
+                'placeholder' => 'The textual body of the page',
+            ],
+        ]);
+
+        $this->crud->addField([
+            'name' => 'thumbnail',
+            'label' => 'Upload an optional image for this blog post',
+            'type' => 'image',
+            'upload' => 'true',
+        ]);
+
+        $this->crud->addField([
+            'name' => 'published',
+            'type' => 'checkbox',
+            'label' => 'Make this post visible to the public?',
+        ]);
 
         // add asterisk for fields that are required in BlogPostRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
