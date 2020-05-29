ed856ac754a7b1f57e8110039186ead109b774e8
Prateek Vasireddy <prateek@dave.com>
2020-05-29 16:27:09 -0700

    radio label

--- resources/views/components/form/_radio.blade.php
+++ resources/views/components/form/_radio.blade.php
@@ -4,14 +4,14 @@
   </div>
 
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="{{ $formInput->formName }}" id="exampleRadios1" value="yes">
-    <label class="form-check-label" for="{{ $formInput->formName }}">
+    <input class="form-check-input" type="radio" name="{{ $formInput->formName }}" id="{{ $formInput->formName }}-1" value="yes">
+    <label class="form-check-label" for="{{ $formInput->formName }}-1">
       Yes
     </label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="{{ $formInput->formName }}" id="exampleRadios2" value="no">
-    <label class="form-check-label" for="{{ $formInput->formName }}">
+    <input class="form-check-input" type="radio" name="{{ $formInput->formName }}" id="{{ $formInput->formName }}-2" value="no">
+    <label class="form-check-label" for="{{ $formInput->formName }}-2">
       No
     </label>
   </div>
