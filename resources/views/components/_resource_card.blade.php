d912d12c23116a8121b78928c0f344a44af929d6
Prateek Vasireddy <prateek@dave.com>
2020-05-29 13:36:04 -0700

    fixed

--- resources/views/components/_resource_card.blade.php
+++ resources/views/components/_resource_card.blade.php
@@ -4,7 +4,7 @@
   <div class="card-body">
     <div class="resource-blurb">
       @if (!empty($item->blurb))
-      <p>{{$item->blurb}}</p>
+        <p>{{$item->blurb}}</p>
       @endif
     </div>
 
@@ -15,54 +15,44 @@
 
       <div class="resource-source">
         @if (!empty($item->source))
-        <a href="{{$item->source}}" target="_blank">Link to source</a>
+          <a href="{{$item->source}}" target="_blank">Link to source</a>
         @endif
       </div>
     </div>
 
     <div class="resource-publication-date">
       @if (!empty($item->publication_date))
-      <p><b>Publication Date:</b> {{$item->publication_date}}</p>
+        <p><b>Publication Date:</b> {{$item->publication_date}}</p>
       @endif
     </div>
 
     <div class="resource-primary-author">
       @if (!empty($item->primary_author))
-      <p><b>Primary Author:</b> {{$item->primary_author}}</p>
+        <p><b>Primary Author:</b> {{$item->primary_author}}</p>
       @endif
     </div>
 
     <div class="resource-secondary-author">
       @if (!empty($item->secondary_author))
-      <p><b>Secondary Author:</b> {{$item->secondary_author}}</p>
+        <p><b>Secondary Author:</b> {{$item->secondary_author}}</p>
       @endif
     </div>
   </div>
 
   <div class="card-footer text-muted">
-<<<<<<< HEAD
     <div class="resource-type">
       <p><b>Type:</b> <span class="badge badge-dark type-badges">{{ $item->item_type->name }}</span></p>
     </div>
 
     <div class="resource-tags">
       @if(count($item->tags) > 0)
-      <p><b>Tags:</b>
-        @foreach($item->tags as $tag)
-        <span class="badge badge-pill badge-dark tag-badges">{{ $tag->name }}</span>
-        @endforeach
-      </p>
+        <p><b>Tags:</b>
+          @foreach($item->tags as $tag)
+            <span class="badge badge-pill badge-dark tag-badges">{{ $tag->name }}</span>
+          @endforeach
+        </p>
       @endif
     </div>
   </div>
 
-=======
-    <span class="badge badge-dark type-badges">{{ $item->item_type->name }}</span>
-    @if(count($item->tags) > 0)
-    @foreach($item->tags as $tag)
-    <span class="badge badge-pill badge-dark tag-badges">{{ $tag->name }}</span>
-    @endforeach
-    @endif
-  </div>
->>>>>>> 9d230631cc477f9835533cf104070c2a892ca403
 </div>
