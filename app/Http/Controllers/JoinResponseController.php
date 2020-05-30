f6e550c25ca527613b0b87094d5660492caf5d4a
Prateek Vasireddy <prateek@dave.com>
2020-05-29 18:55:09 -0700

    cleanup

--- app/Http/Controllers/JoinResponseController.php
+++ app/Http/Controllers/JoinResponseController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\JoinResponse;
-use Illuminate\Http\Request;
 
 class JoinResponseController extends Controller
 {
     public function store()
     {
         $jsonResponse = request()->except(['_token', 'page_id']);
-//        dd($jsonResponse);
 
         JoinResponse::create([
             'page_id' => request()->page_id,
